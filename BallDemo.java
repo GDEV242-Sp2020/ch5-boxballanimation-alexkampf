8fa9c403c84a4a8bfa5e928601f974223f215385
alexkampf <alexk764@gmail.com>
2020-03-01 23:57:13 -0500

    Update BallDemo.java

--- BallDemo.java
+++ BallDemo.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
-
+import java.util.HashSet;
+import java.util.Random;
 /**
  * Class BallDemo - a short demonstration showing animation with the 
  * Canvas class. 
@@ -9,11 +10,21 @@ import java.awt.Color;
  *
  * @author Michael Kölling and David J. Barnes
  * @version 2011.07.31
+ * modified by Alexander Kampf
  */
 
 public class BallDemo   
 {
     private Canvas myCanvas;
+    
+    int boxHeight = 600;
+    int boxWidth = 550;
+    int canvasHeight = 700;
+    int canvasWidth = 700;
+    int xPos = 10;
+    int yPos= 10;
+    HashSet<BoxBall> balls = new HashSet<BoxBall>();
+    private Random r;
 
     /**
      * Create a BallDemo object. Creates a fresh canvas and makes it visible.
@@ -21,36 +32,54 @@ public class BallDemo
     public BallDemo()
     {
         myCanvas = new Canvas("Ball Demo", 600, 500);
+        r = new Random();
     }
 
     /**
-     * Simulate two bouncing balls
+     * Simulate numberofBalls to bounce
      */
-    public void bounce()
+    public void bounce(int numberofBalls)
     {
-        int ground = 400;   // position of the ground line
-
         myCanvas.setVisible(true);
-
-        // draw the ground
-        myCanvas.drawLine(50, ground, 550, ground);
-
-        // crate and show the balls
-        BouncingBall ball = new BouncingBall(50, 50, 16, Color.BLUE, ground, myCanvas);
-        ball.draw();
-        BouncingBall ball2 = new BouncingBall(70, 80, 20, Color.RED, ground, myCanvas);
-        ball2.draw();
-
-        // make them bounce
+        boxBounce(numberofBalls);
         boolean finished =  false;
         while(!finished) {
             myCanvas.wait(50);           // small delay
-            ball.move();
-            ball2.move();
-            // stop once ball has travelled a certain distance on x axis
-            if(ball.getXPosition() >= 550 || ball2.getXPosition() >= 550) {
-                finished = true;
+           for(BoxBall boxBall : balls)
+           {
+               boxBall.move();
+               if(boxBall.getXPosition() >= boxHeight +
+                                     30 * numberofBalls)
+               {
+                   finished = true;
+               }
+           }
+            
             }
         }
+        private void boxBounce(int numberofBalls)
+        {
+            myCanvas.drawRectangle(xPos, yPos, boxWidth,
+                                             boxHeight);
+            //add balls to the set
+            for(int i = 0; i < numberofBalls; i++)
+            {
+                int x = randomPosition();
+                int y = randomPosition();
+                BoxBall boxBall = new BoxBall(x,y,16,Color.BLUE, boxWidth,
+                                                    boxHeight, myCanvas);
+                balls.add(boxBall);
+                boxBall.draw();
+            }
+        }
+        
+        /**
+         * 
+         */
+        private int randomPosition()
+        {//had to parse as int for some reason
+            int pos = boxHeight;
+            return r.nextInt(pos);
+        }
     }
-}
+
